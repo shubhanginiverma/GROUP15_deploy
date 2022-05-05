45b0f9352496d5cb932ffbae21cc9abb398193a8
Shubhangini Verma <shubhanginiverma221@gmail.com>
2022-05-05 22:58:37 +0530

    Update icu.js

--- src/pages/icu.js
+++ src/pages/icu.js
@@ -6,7 +6,7 @@ const ICU = () => {
   return (
     <Layout>
       <div className="container py-5">
-        <h2 className="pb-2 border-bottom text-center">CRITICAL CARE UNIT</h2>
+        <h2 className="pb-2 border-bottom text-center">VITAL MONITORING DASHBOARD</h2>
         <div class = "col-md-12 text-center">
         <a href = "https://public.tableau.com/views/HealthMonitoringTool-WeeklyMonthlyViews_16491824554860/HealthMonitoringDashboard?%3Adisplay_static_image=y&%3AbootstrapWhenNotified=true&%3Aembed=true&%3Alanguage=en-US&:embed=y&:showVizHome=n&:apiID=host0#navType=2&navSrc=Parse"
         target = "_blank">
